f6913c0781908061c1a032ecebb246cc196b299d
Sean Lin <seanlin1016@gmail.com>
2017-12-09 17:01:45 +0800

    first commit

--- FacebookIconAnimation/ViewController.swift
+++ FacebookIconAnimation/ViewController.swift
@@ -12,14 +12,89 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let curvedView = CurvedView()
+//        curvedView.backgroundColor = .yellow
+//        view.addSubview(curvedView)
+//        curvedView.frame = view.frame
+        
+//        let imageView = UIImageView(image: #imageLiteral(resourceName: "thumbs_up"))
+//        let dimension = drand48() * 10 + 20
+//        imageView.frame = CGRect(x: 0, y: 0, width: dimension, height: dimension)
+//
+//        let animation = CAKeyframeAnimation(keyPath: "position")
+//        animation.duration = 2
+//        animation.path = customPath().cgPath
+//        animation.fillMode = kCAFillModeForwards
+//        animation.isRemovedOnCompletion = false
+//        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+//
+//        imageView.layer.add(animation, forKey: nil)
+//
+//        view.addSubview(imageView)
+        let backgroundImageView = UIImageView(image: #imageLiteral(resourceName: "facebookLive"))
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.clipsToBounds = true
+        
+        view.addSubview(backgroundImageView)
+        backgroundImageView.frame = CGRect(x: 0, y: 20, width: view.frame.width, height: view.frame.height)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func handleTap() {
+        (0...10).forEach { (_) in
+            generateAnimationView()
+        }
     }
+    
+    fileprivate func generateAnimationView() {
+        
+        let image = drand48() > 0.5 ? #imageLiteral(resourceName: "thumbs_up") : #imageLiteral(resourceName: "heart")
+        let imageView = UIImageView(image: image)
+        let dimension = drand48() * 10 + 20
+        imageView.frame = CGRect(x: 0, y: 0, width: dimension, height: dimension)
+        
+        let animation = CAKeyframeAnimation(keyPath: "position")
 
+        animation.duration = 2 + drand48() * 3
+        animation.path = customPath().cgPath
+        animation.fillMode = kCAFillModeForwards
+        animation.isRemovedOnCompletion = false
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        
+        imageView.layer.add(animation, forKey: nil)
+        
+        view.addSubview(imageView)
+    }
+}
 
+func customPath() -> UIBezierPath {
+    let path = UIBezierPath()
+    
+    path.move(to: CGPoint(x: 0, y: 200))
+    
+    let endPoint = CGPoint(x: 500, y: 200)
+    
+    let randomYshift = 200 + drand48() * 300
+    let cp1 = CGPoint(x: 100, y: 100 - randomYshift)
+    let cp2 = CGPoint(x: 200, y: 300 + randomYshift)
+    
+    path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+    
+    return path
+}
+
+class CurvedView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        //do some fancy curve drawing
+        
+        let path = customPath()
+        path.lineWidth = 3
+        path.stroke()
+    }
+    
 }
 
